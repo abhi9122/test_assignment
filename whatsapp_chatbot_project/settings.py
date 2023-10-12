19023ec3fdfed34841ac9384bb0070796a361f83
abhi9122 <75219190+abhi9122@users.noreply.github.com>
2023-10-13 02:01:51 +0530

    Update settings.py

--- whatsapp_chatbot_project/settings.py
+++ whatsapp_chatbot_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8)*@v=wohah=ef+bqt6486(=@z!6awk192hg7f6n$_ris(r-2$"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
